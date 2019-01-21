b025b0d1cb8b97c999a8f2865cca993794bb0365
Natalia Ciosek <n_ciosek@wp.pl>
2019-01-21 15:11:28 +0100

    Zmiana gridu

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,35 +1,42 @@
 <template>
-  <div class="container">
-    <div id="app">
-      <v-app id="inspire">
-        <v-container fluid grid-list-md fill-height>
-          <v-layout row wrap>
-            <v-flex d-flex xs6>
-              <v-card color="pink" dark justify-center>
-                <v-card-text class="px-0 text-lg-center justify center">George Bluth<br>
-                  <img src="https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg">
-                </v-card-text>
-              </v-card>
+    <v-container fluid grid-list-md text-xs-center>
+        <v-layout row wrap >
+            <v-flex d-flex xs6 md6>
+
+                <v-card color="blue" light>
+                    <div style="text-align: center;margin-top: 5%">
+                        <p >Text</p>
+                    </div>
+                    <div style="margin-top: 50%;margin-left: 1%;
+              margin-right: 1%">
+                        <p class="text-xs-left">Text
+                            <span style="float:right;">Text</span>
+                        </p>
+                        <img src="https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg">
+                    </div>
+                    <div style="margin-top: 95%;margin-bottom: 5%">
+                        <p class="text-xs-center">Text</p>
+                    </div>
+                </v-card>
             </v-flex>
-            <v-flex d-flex xs6>
-              <v-layout row wrap>
-                <v-flex v-for="i in 12" :key="i" xs6>
-                  <v-card color="blue" dark tile>
-                    <v-card-text class="px-0 text-lg-center">Janet Weaver<br><img src="https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg"></v-card-text>
-                  </v-card>
+
+            <v-flex d-flex xs3 md3 v-for="n in 2"
+                    :key="n">
+                <v-flex d-flex>
+                    <v-layout row wrap>
+                        <v-flex d-flex v-for="n in 6" xs12
+                                :key="n">
+                            <v-card color="pink" light>
+                                <v-card-text>Text</v-card-text>
+                                <img src="https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg">
+                            </v-card>
+                        </v-flex>
+                    </v-layout>
                 </v-flex>
-              </v-layout>
-              <v-flex xs12 sm4 text-xs-center>
-                <div>
-                  <v-btn large v-on:click="toOddNumber">Odd</v-btn>
-                </div>
-              </v-flex>
+
             </v-flex>
-          </v-layout>
-        </v-container>
-      </v-app>
-    </div>
-  </div>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
